b55a39f803dbb5906ba591ed0c180f1670cd94ea
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-12-14 18:46:18 -0500

    Prueba final

--- reto4/src/main/java/com/mintic/reto4/Reto4Application.java
+++ reto4/src/main/java/com/mintic/reto4/Reto4Application.java
@@ -1,13 +1,35 @@
 package com.mintic.reto4;
 
+import com.mintic.reto4.repository.crud.CosmeticsCrudRepository;
+import com.mintic.reto4.repository.crud.OrderCrudRepository;
+import com.mintic.reto4.repository.crud.UserCrudRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
-public class Reto4Application {
+public class Reto4Application implements CommandLineRunner{
+
+	@Autowired
+    private OrderCrudRepository orderCrudRepository;
+    @Autowired
+    private UserCrudRepository userCrudRepository;
+    @Autowired
+    private CosmeticsCrudRepository cosmeticsCrudRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Reto4Application.class, args);
 	}
+
+	@Override
+    public void run(String... args) throws Exception {
+        cosmeticsCrudRepository.deleteAll();
+        userCrudRepository.deleteAll();
+        orderCrudRepository.deleteAll();
+    }
 
 }
