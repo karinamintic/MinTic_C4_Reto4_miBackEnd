97733bfe4f45a3d42640ad6a61099b6e03f3365e
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-12-14 19:07:37 -0500

    Prueba fix error

--- reto4/src/main/java/com/mintic/reto4/repository/crud/CosmeticsCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mintic.reto4.repository.crud;
-
-import com.mintic.reto4.model.Cosmetics;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CosmeticsCrudRepository extends MongoRepository<Cosmetics, String> {
-
-}
